c0cfbff1b10f5f984c1ac4866efcdcaee5271bcb
段金磊 <3001720396@qq.com>
2018-09-29 17:42:27 +0800

    开始编辑页面

---
+++ app/src/main/java/com/jimduan/classbell/activity/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.jimduan.classbell.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import com.jimduan.classbell.R
+import com.jimduan.classbell.utils.MyActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class SplashActivity : MyActivity() {
+    val handler = Handler()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        supportActionBar?.hide()
+        fullscreen_content.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        handler.postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1000)
+    }
+
+
+}
---
+++ app/src/main/java/com/jimduan/classbell/utils/MyActivity.kt
@@ -0,0 +1,10 @@
+package com.jimduan.classbell.utils
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by DJl on 2018/9/29.
+ * email:1554068430@qq.com
+ */
+open class MyActivity : AppCompatActivity() {
+}
